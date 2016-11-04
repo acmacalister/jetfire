d2184a3990c96716d2658f9b8d3c06c2c6a898cf
Grzegorz Nowicki <gnowicki@egnyte.com>
2016-11-04 13:29:27 +0100

    Carthage support - fixed Mac build

---
+++ Jetfire-Mac/Jetfire-Mac.h
@@ -0,0 +1,19 @@
+//
+//  Jetfire-Mac.h
+//  Jetfire-Mac
+//
+//  Created by Grzegorz Nowicki on 04.11.2016.
+//  Copyright © 2016 Vluxe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Jetfire-Mac.
+FOUNDATION_EXPORT double Jetfire_MacVersionNumber;
+
+//! Project version string for Jetfire-Mac.
+FOUNDATION_EXPORT const unsigned char Jetfire_MacVersionString[];
+
+#import <Jetfire/JFRWebSocket.h>
+#import <Jetfire/JFRSecurity.h>
+
